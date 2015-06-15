752f928fac77cd0e018cfa9a376dbe9094c03515
Yuta Hayashibe <yuta@hayashibe.jp>
2015-06-15 10:17:50 +0900

    Trimmed test scripts

--- convert/knp_test.go
+++ convert/knp_test.go
@@ -9,61 +9,74 @@ import (
 	"testing"
 )
 
-func TestWritePlainText(t *testing.T) {
-
-	inf, err := os.Open("knp_test/input.knp")
-	if err != nil {
-		t.Errorf("Error: %v", err)
-		return
-	}
-	defer inf.Close()
-
-	//make output
-	//cf: http://stackoverflow.com/questions/10473800/
-	old := os.Stdout // keep backup of the real stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	err = ConvertKNP(inf, os.Stdout, true)
-	if err != nil {
-		t.Errorf("Error: %s", err)
-		return
+func TestConvertKNP(t *testing.T) {
+	tests := []struct {
+		input    string
+		gold     string
+		tokenize bool
+	}{
+		{
+			input:    "knp_test/input.knp",
+			gold:     "knp_test/tokenized_gold.txt",
+			tokenize: true,
+		},
 	}
 
-	outC := make(chan string)
-	// copy the output in a separate goroutine so printing can't block indefinitely
-	go func() {
-		var buf bytes.Buffer
-		io.Copy(&buf, r)
-		outC <- buf.String()
-	}()
+	for _, test := range tests {
+		inf, err := os.Open(test.input)
+		if err != nil {
+			t.Errorf("Error: %v", err)
+			return
+		}
+		defer inf.Close()
 
-	// back to normal state
-	w.Close()
-	os.Stdout = old // restoring the real stdout
-	gotStdout := <-outC
+		//make output
+		//cf: http://stackoverflow.com/questions/10473800/
+		old := os.Stdout // keep backup of the real stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
 
-	//get golds
-	goldf, err := os.Open("knp_test/gold.txt")
-	gold_reader := bufio.NewReader(goldf)
-	if err != nil {
-		t.Errorf("Error when open the gold file: %v", err)
-		return
-	}
-
-	//Check
-	for _, line := range strings.Split(gotStdout, "\n") {
-		_gold_line, _, err := gold_reader.ReadLine()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			t.Errorf("Error when getting gold line: %s", err)
+		err = ConvertKNP(inf, os.Stdout, test.tokenize)
+		if err != nil {
+			t.Errorf("Error: %s", err)
 			return
 		}
 
-		gold_line := string(_gold_line)
-		if line != gold_line {
-			t.Errorf("got\n[%s]\nbut want\n[%s]\n", line, gold_line)
+		outC := make(chan string)
+		// copy the output in a separate goroutine so printing can't block indefinitely
+		go func() {
+			var buf bytes.Buffer
+			io.Copy(&buf, r)
+			outC <- buf.String()
+		}()
+
+		// back to normal state
+		w.Close()
+		os.Stdout = old // restoring the real stdout
+		gotStdout := <-outC
+
+		//get golds
+		goldf, err := os.Open(test.gold)
+		gold_reader := bufio.NewReader(goldf)
+		if err != nil {
+			t.Errorf("Error when open the gold file: %v", err)
+			return
+		}
+
+		//Check
+		for _, line := range strings.Split(gotStdout, "\n") {
+			_gold_line, _, err := gold_reader.ReadLine()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				t.Errorf("Error when getting gold line: %s", err)
+				return
+			}
+
+			gold_line := string(_gold_line)
+			if line != gold_line {
+				t.Errorf("got\n[%s]\nbut want\n[%s]\n", line, gold_line)
+			}
 		}
 	}
 }
